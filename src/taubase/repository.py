744a7697794c842f4f8ed54c3d3422c3a4948b26
Charles Tapley Hoyt <cthoyt@gmail.com>
2019-07-02 16:27:20 +0200

    Reorganize getting graphs

--- src/taubase/repository.py
+++ src/taubase/repository.py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 
-""""""
+"""Graph getting functions."""
 
-import hbp_knowledge
+import logging
+
+import epilepsy_knowledge
 import hbp_enrichment
+import hbp_knowledge
+import hbp_semi_automated_curation
 import neurommsig_alzheimers_knowledge
 import neurommsig_parkinsons_knowledge
-import epilepsy_knowledge
-import hbp_semi_automated_curation
-import logging
 from pybel import BELGraph, union
 
 __all__ = [
@@ -20,21 +21,41 @@ logger = logging.getLogger(__name__)
 
 def get_graph() -> BELGraph:
     """Get the graph from all sources."""
-    logger.info('getting HBP')
+    graphs = []
+
+    logger.info('getting pharmacome/knowledge')
     hbp_graph = hbp_knowledge.get_graph()
+    graphs.append(hbp_graph)
+
+    logger.info('getting pharmacome/semi-automated-curation')
     hbp_semi_automated_graph = hbp_semi_automated_curation.repository.get_graph()
+    graphs.append(hbp_semi_automated_graph)
+
+    logger.info('getting pharmacome/knowledge')
     hbp_enrichment_graph = hbp_enrichment.repository.get_graph()
+    graphs.append(hbp_enrichment_graph)
 
-    logger.info('getting NeuroMMSig')
-    neurommsig_alzheimers_graph = neurommsig_alzheimers_knowledge.repository.get_graph()
-    neurommsig_parkinsons_graph = neurommsig_parkinsons_knowledge.repository.get_graph()
+    return union(graphs)
+
+
+def get_neurommsig_graph():
+    graphs = []
+    logger.info('getting neurommsig-epilepsy/knowledge')
     neurommsig_epilepsy_graph = epilepsy_knowledge.repository.get_graph()
+    graphs.append(neurommsig_epilepsy_graph)
 
-    return union([
-        hbp_graph,
-        hbp_semi_automated_graph,
-        hbp_enrichment_graph,
-        neurommsig_alzheimers_graph,
-        neurommsig_parkinsons_graph,
-        neurommsig_epilepsy_graph,
-    ])
+    try:
+        neurommsig_alzheimers_graph = neurommsig_alzheimers_knowledge.repository.get_graph()
+    except ImportError:
+        logger.info('skipping neurommsig-alzheimers')
+    else:
+        graphs.append(neurommsig_alzheimers_graph)
+
+    try:
+        neurommsig_parkinsons_graph = neurommsig_parkinsons_knowledge.repository.get_graph()
+    except ImportError:
+        logger.info('skipping neurommsig-parkinsons')
+    else:
+        graphs.append(neurommsig_parkinsons_graph)
+
+    return union(graphs)
